b76e382733650fa5a1301c9f5ff47fa510cd5c47
Edestuss0 <bogdan.romanenia@gmail.com>
2026-02-03 16:46:02 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,45 @@
 const express = require("express");
 const fs = require("fs");
-const cors = require("cors"); // <- добавили
+const cors = require("cors");
 
 const app = express();
 const PORT = 3001;
 
-app.use(cors()); // <- включили CORS
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
 
+
+const DATA_PATH = "data/objects.json";
+
+// GET — получить все объекты
 app.get("/api/data", (req, res) => {
-    const json = fs.readFileSync("data/objects.json", "utf-8");
+    const json = fs.readFileSync(DATA_PATH, "utf-8");
     res.json(JSON.parse(json));
 });
 
+// POST — добавить новый объект
+app.post("/api/data", (req, res) => {
+    const newObject = req.body;
+
+    if (!newObject) {
+        return res.status(400).json({ error: "Нет данных" });
+    }
+
+    const json = fs.readFileSync(DATA_PATH, "utf-8");
+    const objects = JSON.parse(json);
+
+    const objectWithId = {
+        id: Date.now(), // 👈 простой уникальный id
+        ...newObject
+    };
+
+    objects.push(objectWithId);
+
+    fs.writeFileSync(DATA_PATH, JSON.stringify(objects, null, 2));
+
+    res.json(objectWithId);
+});
+
 app.listen(PORT, () => {
-    console.log(`API запущен: http://localhost:${PORT}/api/data`);
+    console.log(`API запущен: http://localhost:${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { MainPage } from "./pages/MainPage.jsx";
+import { AddObjectPage } from "./pages/AddObjectPage.jsx";
 
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
       <Routes>
-          <Route path="*" element={<MainPage />} />\
+          <Route path="/" element={<MainPage />} />
+          <Route path="/add" element={<AddObjectPage />} />
       </Routes>
         
     </>
